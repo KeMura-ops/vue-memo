7809bec8414491b340cd9636d13ef64313a05edf
KeMura-ops <kenti432@yahoo.co.jp>
2021-09-12 16:03:21 +0900

    スタイルの適用

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -56,9 +56,50 @@ export default {
 }
 </script>
 
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
+<style lang="scss" scoped>
+  .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 32px;
+
+    &-group {
+      margin-bottom: 1rem;
+    }
+
+    &-control {
+      width: 400px;
+      min-height: 24px;
+      font-size: 1rem;
+      border: 1px solid #ced4da;
+      padding: 4px 8px;
+    }
+
+    & button {
+      width: 200px;
+    }
+  }
+
+  .flex {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    & .card {
+      width: 238px;
+      border: 1px solid rgba(0, 0, 0, .125);
+      border-radius: .25rem;
+      margin: 16px;
+
+      &-body {
+        padding: 1.25rem;
+      }
+
+      &-title {
+        margin-bottom: .75rem;
+        font-weight: bold;
+      }
+    }
+  }
 </style>
